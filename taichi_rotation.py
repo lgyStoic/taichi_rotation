7da842236225c5b5a2376caf3a72adf77703173a
lgystoic <linggaoyuan@gmail.com>
2022-08-26 21:08:32 +0800

    feat: add saa

--- taichi_rotation.py
+++ taichi_rotation.py
@@ -1,89 +1,94 @@
 # reference ==>
 
+from operator import truediv
 import taichi as ti
 
-ti.init(arch = ti.cuda)
+ti.init(arch = ti.gpu)
 
 res_x = 512
 res_y = 512
 r_big = 200.0
+r_part =  100.0 
+r_small = 40.0
+r_out = 205.0
+
 pixels = ti.Vector.field(3, ti.f32, shape=(res_x, res_y))
+
+
+
 
 @ ti.func
 def circle(pos, center, radius):
     r = (pos - center).norm()
     t = 0
-    if r < radius:
+    if r <= radius:
         t = 1
     return t
-@ti.func
-def smoothstep(edge0, edge1, x):
-    t = (x - edge0) / (edge1 - edge0)
-    t = ti.max(0, ti.min(1, t))
-    return (3 - 2 * t) * t * t
-
 
 
 @ti.kernel
-def render(t:ti.f32):
-    center = ti.Vector([res_x//2, res_y//2])
+def render(t:ti.f32, scale:float):
+    r_big = r_big / scale
+    r_part = r_part / scale
+    r_small = r_small / scale
+    r_out = r_out / scale
+    center = ti.Vector([res_x//2, res_y//2]) / scale
 
 
     center_up = ti.Vector([center[0], (center[1] - r_big + center[1]) / 2])
     center_down = ti.Vector([center[0], (center[1] + r_big + center[1]) / 2])
-    r_part =  100.0 
-    r_small = 40.0
-    r_out = 205.0
+    
     for i,j in pixels:
         color = ti.Vector([0.0, 0.0, 0.0]) # init your canvas to white
-        pos = ti.Vector([i, j])
-        
-        c = circle(pos, center, r_big)
-        c_up_small = circle(pos, center_up, r_small)
-        c_down_small =  circle(pos, center_down, r_small)
-        c_up_big = circle(pos, center_up, r_part)
-        c_down_big = circle(pos, center_down, r_part)
-        c_out = circle(pos, center, r_out)
-        if c_up_small:
-            color = ti.Vector([1.0, 1.0, 1.0])
-        elif c_down_small:
-            color = ti.Vector([0.0, 0.0, 0.0])
-        elif c_up_big:
-            color = ti.Vector([0.0, 0.0, 0.0])
-        elif c_down_big:
-            color = ti.Vector([1.0, 1.0, 1.0])
-        elif c:
-            if pos[0] < center[0]:
-                color = ti.Vector([0.0, 0.0, 0.0])
-            else:
-                color = ti.Vector([1.0, 1.0, 1.0])
-        elif c_out:
-            color = ti.Vector([0.0, 0.0, 0.0])
-        else:
-            color = ti.Vector([1.0, 1.0, 1.0])
+        posScale = ti.Vector([i, j]) / scale
+        sumColor = ti.math.vec3(0.0, 0.0, 0.0)
+        for m in range(0, ti.cast(1 / scale, ti.int32)):
+            for n in range(0, ti.cast(1 / scale, ti.int32)):
+                pos = ti.math.vec2([posScale[0] + m], posScale[1] + n)
 
+                c = circle(pos, center, r_big)
+                c_up_small = circle(pos, center_up, r_small)
+                c_down_small =  circle(pos, center_down, r_small)
+                c_up_big = circle(pos, center_up, r_part)
+                c_down_big = circle(pos, center_down, r_part)
+                c_out = circle(pos, center, r_out)
+                if c_up_small:
+                    color = ti.Vector([1.0, 1.0, 1.0])
+                elif c_down_small:
+                    color = ti.Vector([0.0, 0.0, 0.0])
+                elif c_up_big:
+                    color = ti.Vector([0.0, 0.0, 0.0])
+                elif c_down_big:
+                    color = ti.Vector([1.0, 1.0, 1.0])
+                elif c:
+                    if pos[0] < center[0]:
+                        color = ti.Vector([0.0, 0.0, 0.0])
+                    else:
+                        color = ti.Vector([1.0, 1.0, 1.0])
+                elif c_out:
+                    color = ti.Vector([0.0, 0.0, 0.0])
+                else:
+                    color = ti.Vector([1.0, 1.0, 1.0])
+                sumColor += color
+        pixels[i, j] = sumColor / (ti.cast(1 / scale, ti.int32) ** 2)
+        center_raw = center * scale
+        r_out_raw = r_out * scale
         # rotation 
-        if not c: 
-            pixels[i,j] = color
-        else:
-            i_ = ti.cast((i - center[0]) * ti.math.cos(t) - (j - center[1]) * ti.math.sin(t) + center[0], ti.int32)
-            j_ = ti.cast((i - center[0]) * ti.math.sin(t) + (j - center[1]) * ti.math.cos(t) + center[1], ti.int32)
-            pixels[i_, j_] = color
-@ti.kernel
-def smooth():
-    center = ti.math.vec2(res_x // 2, res_y // 2)
-    for i,j in pixels:
-        r = (ti.math.vec2(i, j) - center).norm()
-        d = r / r_big
-        pixels[i, j] = smoothstep(0.9, 1, d) * pixels[i, j]
+        # if circle(ti.math.vec2(i, j), center_raw, r_out_raw) != 0:
+        #     i_ = ti.cast((i - center_raw[0]) * ti.math.cos(t) - (j - center_raw[1]) * ti.math.sin(t) + center_raw[0], ti.int32)
+        #     j_ = ti.cast((i - center_raw[0]) * ti.math.sin(t) + (j - center_raw[1]) * ti.math.cos(t) + center_raw[1], ti.int32)
+        #     pixels[i_, j_] = sumColor / (ti.cast(1 / scale, ti.int32) ** 2)
+        # else:
+        #     pixels[i, j] = ti.math.vec3(1.0, 1.0, 1.0)
 
 
 
 gui = ti.GUI("taichi logo", res=(res_x, res_y))
-
-for i in range(59900):
+run = False
+for i in range(10000000000000):
     t = i * 0.01
-    render(t)
-    smooth()
-    gui.set_image(pixels)
-    gui.show()
+    if not run:
+        run = True
+        render(t, 1.0/1024)
+        gui.set_image(pixels)
+        gui.show()
